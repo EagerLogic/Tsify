06755c675ff12a5c452bc6e3680d00338c376d38
David Ipacs <david.ipacs@gmail.com>
2019-01-20 10:39:40 +0100

    creating composite exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export function a() {
-    
-}
+export { AEntity } from './AEntity';
+export { IEntityFactory } from './IEntityFactory';
+export { TId, Key } from './Key';
+export { Class, IJob, Tsify, FilterOperator, Finder } from './Tsify';
